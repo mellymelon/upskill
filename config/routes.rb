99ef0cd8ac6061d4bbd946f5f4834ef8a6bb5253
Yong Liang <liangyong0411@gmail.com>
2018-07-21 08:13:22 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resource :profile
+  end
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
